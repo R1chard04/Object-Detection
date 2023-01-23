e491bc522a1bee29891649f1865f0d020ba39f85
Leoy456 <leoyou2004@gmail.com>
2023-01-23 15:57:02 -0500

    things

--- Image-Processing/compareImages.py
+++ Image-Processing/compareImages.py
@@ -5,15 +5,19 @@ from os import listdir
 import sys
 
 # load the input images
-off1 = cv2.imread('Image-Processing\photos\Test\Reference\WIN_20230123_10_46_25_Pro.jpg')
-off2 = cv2.imread('Image-Processing\photos\Test\Fault\WIN_20230123_10_46_55_Pro.jpg')
+off1 = cv2.imread('Image-Processing\photos\Test\Fault\WIN_20230123_10_47_49_Pro.jpg')
 standard = cv2.imread('Image-Processing\photos\Test\Reference\WIN_20230123_10_46_27_Pro.jpg')
-cv2.imshow("diff2", off1)
 
+
+alpha = 3 # Contrast control (1.0-3.0)
+beta = 0 # Brightness control (0-100)
+off1 = cv2.convertScaleAbs(off1, alpha=alpha, beta=beta)
+standard = cv2.convertScaleAbs(standard, alpha=alpha, beta=beta)
 # convert the images to grayscale
 off1 = cv2.cvtColor(off1, cv2.COLOR_BGR2GRAY)
-off2 = cv2.cvtColor(off2, cv2.COLOR_BGR2GRAY)
 standard = cv2.cvtColor(standard, cv2.COLOR_BGR2GRAY)
+cv2.imshow('adjustedX', off1)
+cv2.imshow('adjustedS', standard)
 
 def mse(img1, img2):
     h, w = img1.shape
@@ -23,28 +27,21 @@ def mse(img1, img2):
     return ans, diff
 
 #Masking
-maskImg = cv2.imread('Image-Processing\photos\Test\Reference\mask.jpg')
+maskImg = cv2.imread('Image-Processing\photos\Test\mask2.jpg')
 grayMask = cv2.cvtColor(maskImg, cv2.COLOR_BGR2GRAY)
 (thresh, maskBW) = cv2.threshold(grayMask, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 thresh = 127
 binaryMask = cv2.threshold(grayMask, thresh, 255, cv2.THRESH_BINARY)[1]
 
-
 off1M = cv2.bitwise_and(off1,off1, mask= binaryMask)
-off2M = cv2.bitwise_and(off2,off2, mask= binaryMask)
 standardM = cv2.bitwise_and(standard,standard, mask= binaryMask)
 
-
 error, diff1 = mse(standardM, off1M)
-error2, diff2 = mse(standardM, off2M)
 
 print("Error between the two images1: ", error)
-print("Error between the two images2: ", error2)
-
-diff1 = cv2.resize(diff1, (0,0), fx = 0.3, fy = 0.3)
-diff = cv2.resize(diff2, (0,0), fx = 0.3, fy = 0.3)
 
 # cv2.imshow("diff1", diff1)
-# cv2.imshow("diff2", diff2)
+# cv2.imshow("off1", off1M)
+# cv2.imshow("standard", standardM)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
--- Image-Processing/test.py
+++ Image-Processing/test.py
@@ -6,11 +6,10 @@ import sys
 
 #-----------------------------------------Importing folders and images-----------------------------------------#
 #Mask
-maskImg = cv.imread('Image-Processing\photos\Test\mask3.jpg')
+maskImg = cv.imread('Image-Processing\photos\Test\mask.jpg')  #No tolerance around piece, 1-4 is increasing in tolerance
 
 #Standard
-refImg = cv.cvtColor(cv.imread('Image-Processing\photos\Test\Reference\STANDARD.jpg'), cv.COLOR_BGR2GRAY)
-
+refImg = cv.imread('Image-Processing\photos\Test\Reference\STANDARD.jpg')
 
 #References
 refDirectory = 'Image-Processing\photos\Test\Reference'
@@ -21,18 +20,25 @@ faultDirectory = 'Image-Processing\photos\Test\Fault'
 #Folders
 folderDirectories = [refDirectory,faultDirectory]
 
-#-----------------------------------------Configuring Mask, MSE, Report-----------------------------------------#
+#-----------------------------------------Configuring Mask, MSE, Filter, Report-----------------------------------------#
 #Mask
 maskImgG = cv.cvtColor(maskImg, cv.COLOR_BGR2GRAY)
 (maskThresh, maskImgBW) = cv.threshold(maskImgG, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
 thresh = 127
 maskImgBinary = cv.threshold(maskImgG, maskThresh, 255, cv.THRESH_BINARY)[1]
 
+#Filter
+alpha = 3 # Contrast control (rec 1-3)
+beta = 0 # Brightness control (rec -300 <-> 300)
+
+refImg = cv.convertScaleAbs(refImg, alpha=alpha, beta=beta)
+refImg = cv.cvtColor(refImg, cv.COLOR_BGR2GRAY)
+
 #Standard Mask
 refImgM = cv.bitwise_and(refImg,refImg, mask = maskImgBinary)
 
 #MSE
-def mse(img1, img2):
+def mse(img1, img2): #mean squared error
     height, width = img1.shape
     diffImg = cv.subtract(img1, img2)
     err = np.sum(diffImg**2)
@@ -47,14 +53,21 @@ report.write("Report of deviations from reference image")
 
 for i in range(len(folderDirectories)):
     report.write("\n\nFrom: ",)
-    imageIndex = 0
+    pieceIndex = 0
 
     for image in os.listdir(folderDirectories[i]):
         imagePath = os.path.join(folderDirectories[i], image)
-        report.write("\n"+ str(imageIndex) + " " + image + ": ")
-        imageIndex += 1
+        report.write("\n"+ str(pieceIndex) + " " + image + ": ")
+        pieceIndex += 1
 
-        inputImg = cv.cvtColor(cv.imread(imagePath), cv.COLOR_BGR2GRAY)
+        inputImg = cv.convertScaleAbs(cv.imread(imagePath), alpha=alpha, beta=beta)
+        inputImg = cv.cvtColor(inputImg, cv.COLOR_BGR2GRAY)
         inputImgM = cv.bitwise_and(inputImg,inputImg, mask= maskImgBinary)
         error, diffImg = mse(refImgM, inputImgM)
+
         report.write(str(error))
+        report.write(" Pass") if error < 0.1 else report.write(" Fail")  
+        #for this mask, the set tolerance is 0.1, this seems to work best, but this is experimentally tested (which isn't awesome)
+
+report.close()
+
