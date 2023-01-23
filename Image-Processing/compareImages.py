36e2c30a8b4036cb825d5ec09d3474fb01dab67a
Leoy456 <leoyou2004@gmail.com>
2023-01-23 13:23:08 -0500

    Loop and new photos

--- Image-Processing/compareImages.py
+++ Image-Processing/compareImages.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
+import os
+from os import listdir
+import sys
 
 # load the input images
-off1 = cv2.imread('Image-Processing\photos\Small\Original\OFF1.jpg')
-off2 = cv2.imread('Image-Processing\photos\Small\Original\OFF6.jpg')
-standard = cv2.imread('Image-Processing\photos\Small\Original\OG1.jpg')
+off1 = cv2.imread('Image-Processing\photos\Test\Reference\WIN_20230123_10_46_25_Pro.jpg')
+off2 = cv2.imread('Image-Processing\photos\Test\Fault\WIN_20230123_10_46_55_Pro.jpg')
+standard = cv2.imread('Image-Processing\photos\Test\Reference\WIN_20230123_10_46_27_Pro.jpg')
+cv2.imshow("diff2", off1)
 
 # convert the images to grayscale
 off1 = cv2.cvtColor(off1, cv2.COLOR_BGR2GRAY)
@@ -15,22 +19,24 @@ def mse(img1, img2):
     h, w = img1.shape
     diff = cv2.subtract(img1, img2)
     err = np.sum(diff**2)
-    ans = err/(float(h*w))
+    ans = err/(float(h*w)) #Closer to 0 is better
     return ans, diff
 
 #Masking
-maskImg = cv2.imread('Image-Processing\photos\Small\Mask.jpg')
+maskImg = cv2.imread('Image-Processing\photos\Test\Reference\mask.jpg')
 grayMask = cv2.cvtColor(maskImg, cv2.COLOR_BGR2GRAY)
 (thresh, maskBW) = cv2.threshold(grayMask, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 thresh = 127
 binaryMask = cv2.threshold(grayMask, thresh, 255, cv2.THRESH_BINARY)[1]
-#binaryMask = cv2.resize(diff1, (0,0), fx = 0.3, fy = 0.3)   
+
+
 off1M = cv2.bitwise_and(off1,off1, mask= binaryMask)
 off2M = cv2.bitwise_and(off2,off2, mask= binaryMask)
 standardM = cv2.bitwise_and(standard,standard, mask= binaryMask)
 
+
 error, diff1 = mse(standardM, off1M)
-error2, diff2 = mse(standardM, standardM)
+error2, diff2 = mse(standardM, off2M)
 
 print("Error between the two images1: ", error)
 print("Error between the two images2: ", error2)
@@ -38,7 +44,7 @@ print("Error between the two images2: ", error2)
 diff1 = cv2.resize(diff1, (0,0), fx = 0.3, fy = 0.3)
 diff = cv2.resize(diff2, (0,0), fx = 0.3, fy = 0.3)
 
-cv2.imshow("diff1", diff1)
-cv2.imshow("diff2", diff2)
+# cv2.imshow("diff1", diff1)
+# cv2.imshow("diff2", diff2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- Image-Processing/test.py
+++ Image-Processing/test.py
@@ -4,67 +4,57 @@ import os
 from os import listdir
 import sys
 
-maskImg = cv.imread('Image-Processing\photos\Small\Mask.jpg')
+#-----------------------------------------Importing folders and images-----------------------------------------#
+#Mask
+maskImg = cv.imread('Image-Processing\photos\Test\mask3.jpg')
 
-folderDirectory = "Image-Processing\photos\Small\Original"
-imageIndex = 0
+#Standard
+refImg = cv.cvtColor(cv.imread('Image-Processing\photos\Test\Reference\STANDARD.jpg'), cv.COLOR_BGR2GRAY)
 
-for image in os.listdir(folderDirectory):
 
-    if (image.endswith(".jpg")):
-        imagePath = os.path.join(folderDirectory, image)
-        print(imageIndex, image, imagePath)
+#References
+refDirectory = 'Image-Processing\photos\Test\Reference'
+
+#Faults
+faultDirectory = 'Image-Processing\photos\Test\Fault'
+
+#Folders
+folderDirectories = [refDirectory,faultDirectory]
+
+#-----------------------------------------Configuring Mask, MSE, Report-----------------------------------------#
+#Mask
+maskImgG = cv.cvtColor(maskImg, cv.COLOR_BGR2GRAY)
+(maskThresh, maskImgBW) = cv.threshold(maskImgG, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+thresh = 127
+maskImgBinary = cv.threshold(maskImgG, maskThresh, 255, cv.THRESH_BINARY)[1]
+
+#Standard Mask
+refImgM = cv.bitwise_and(refImg,refImg, mask = maskImgBinary)
+
+#MSE
+def mse(img1, img2):
+    height, width = img1.shape
+    diffImg = cv.subtract(img1, img2)
+    err = np.sum(diffImg**2)
+    ans = err/(float(height*width)) #Closer to 0 is better
+    return ans, diffImg
+
+#Report
+report = open("report.txt", "w")
+report.write("Report of deviations from reference image")
+
+#-----------------------------------------Main Loop-----------------------------------------#
+
+for i in range(len(folderDirectories)):
+    report.write("\n\nFrom: ",)
+    imageIndex = 0
+
+    for image in os.listdir(folderDirectories[i]):
+        imagePath = os.path.join(folderDirectories[i], image)
+        report.write("\n"+ str(imageIndex) + " " + image + ": ")
         imageIndex += 1
 
-        img = cv.imread(imagePath)
-
-        grayMask = cv.cvtColor(maskImg, cv.COLOR_BGR2GRAY)
-        (thresh, im_bw) = cv.threshold(grayMask, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-        thresh = 127
-        binaryMask = cv.threshold(grayMask, thresh, 255, cv.THRESH_BINARY)[1]
-
-        result = cv.bitwise_and(img,img, mask= binaryMask)
-        cv.imshow("masked",result)
-
-        # scale_percent = 50 # percent of original size
-        # width = int(img.shape[1] * scale_percent / 100)
-        # height = int(img.shape[0] * scale_percent / 100)
-        # dim = (width, height)
-        # img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
-
-        # alpha = 3 # Contrast control (1.0-3.0)
-        # beta = 0 # Brightness control (0-100)
-        # adjusted = cv.convertScaleAbs(img, alpha=alpha, beta=beta)
-        # cv.imshow('adjusted', adjusted)
-        
-        # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        # dns = cv.fastNlMeansDenoising(gray,None,15,7,21)
-        
-  
-        # # Find Canny edges
-        # edged = cv.Canny(dns, 30, 200)
-        
-        # Finding Contours
-        # Use a copy of the image e.g. edged.copy()
-        # since findContours alters the image
-        # contours, hierarchy = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-        
-        # print("Number of Contours found = " + str(len(contours)))
-        
-        # # Draw all contours
-        # # -1 signifies drawing all contours
-        # cv.drawContours(dns, contours, -1, (0, 255, 0), 2)
-        
-        # cv.imshow('Contours', dns)
-
-        # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        
-
-        # cv.imshow('img', show)
-        cv.waitKey(0)
-
-# # Edge Cascade
-# canny = cv2.Canny(adjusted, 15, 175)
-# cv2.imshow('Canny Edges', canny)
-
-# cv2.waitKey(0)
+        inputImg = cv.cvtColor(cv.imread(imagePath), cv.COLOR_BGR2GRAY)
+        inputImgM = cv.bitwise_and(inputImg,inputImg, mask= maskImgBinary)
+        error, diffImg = mse(refImgM, inputImgM)
+        report.write(str(error))
