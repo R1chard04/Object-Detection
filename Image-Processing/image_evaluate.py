e6c2f9fe5097b99754756d0b64f299a232040b0a
Leoy456 <leoyou2004@gmail.com>
2023-01-24 11:08:54 -0500

    need pull

--- Image-Processing/image_evaluate.py
+++ Image-Processing/image_evaluate.py
@@ -3,6 +3,11 @@ import numpy as np
 import os
 from os import listdir
 import sys
+import time
+import datetime
+from pathlib import Path
+import depthai as dai
+import os.path
 
 #-----------------------------------------Importing folders and images-----------------------------------------#
 #Mask
@@ -20,7 +25,7 @@ faultDirectory = 'Image-Processing\photos\Test\Fault'
 #Folders
 folderDirectories = [refDirectory,faultDirectory]
 
-#-----------------------------------------Configuring Mask, MSE, Filter, Report-----------------------------------------#
+#-----------------------------------------Configuring Mask, MSE, Filter, Comparison, Report-----------------------------------------#
 #Mask
 maskImgG = cv.cvtColor(maskImg, cv.COLOR_BGR2GRAY)
 (maskThresh, maskImgBW) = cv.threshold(maskImgG, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
@@ -45,29 +50,112 @@ def mse(img1, img2): #mean squared error
     ans = err/(float(height*width)) #Closer to 0 is better
     return ans, diffImg
 
+#Comparison
+def compare(testPath):
+    report.write("\n"+testPath)
+    testImg = cv.convertScaleAbs(cv.imread(testPath), alpha=alpha, beta=beta)
+    testImg = cv.cvtColor(testImg, cv.COLOR_BGR2GRAY)
+    inputImgM = cv.bitwise_and(testImg,testImg, mask= maskImgBinary)
+    error, diffImg = mse(refImgM, inputImgM)
+
+    report.write(str(error))
+    report.write(" Pass") if error < 0.1 else report.write(" Fail")  
+    # for this mask, the set tolerance is 0.1, this seems to work best, but this is experimentally tested (which isn't awesome)
+
 #Report
 report = open("report.txt", "w")
 report.write("Report of deviations from reference image")
 
+#-----------------------------------------Configuring Camera and Pictures Directory-----------------------------------------#
+#Camera config
+# Create pipeline
+pipeline = dai.Pipeline()
+
+camRgb = pipeline.create(dai.node.ColorCamera)
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+xoutRgb.setStreamName("rgb")
+camRgb.video.link(xoutRgb.input)
+
+xin = pipeline.create(dai.node.XLinkIn)
+xin.setStreamName("control")
+xin.out.link(camRgb.inputControl)
+
+# Properties
+videoEnc = pipeline.create(dai.node.VideoEncoder)
+videoEnc.setDefaultProfilePreset(1, dai.VideoEncoderProperties.Profile.MJPEG)
+camRgb.still.link(videoEnc.input)
+
+# Linking
+xoutStill = pipeline.create(dai.node.XLinkOut)
+xoutStill.setStreamName("still")
+videoEnc.bitstream.link(xoutStill.input)
+
+#Photos directory 
+dirName = "Image-Processing\photos\Input"
+Path(dirName).mkdir(parents=True, exist_ok=True)
+
+#Pictures Name Generator
+def timeStamp():
+    now = str(datetime.datetime.now())
+    now = round(float(((now.replace("-","")).replace(" ","")).replace(":","")))
+    return now
+
 #-----------------------------------------Main Loop-----------------------------------------#
 
-for i in range(len(folderDirectories)):
-    report.write("\n\nFrom: ",)
-    pieceIndex = 0
+with dai.Device(pipeline) as device:
 
-    for image in os.listdir(folderDirectories[i]):
-        imagePath = os.path.join(folderDirectories[i], image)
-        report.write("\n"+ str(pieceIndex) + " " + image + ": ")
-        pieceIndex += 1
+    # Output queue will be used to get the rgb frames from the output defined above
+    qRgb = device.getOutputQueue(name="rgb", maxSize=30, blocking=False)
+    qStill = device.getOutputQueue(name="still", maxSize=30, blocking=True)
+    qControl = device.getInputQueue(name="control")
 
-        inputImg = cv.convertScaleAbs(cv.imread(imagePath), alpha=alpha, beta=beta)
-        inputImg = cv.cvtColor(inputImg, cv.COLOR_BGR2GRAY)
-        inputImgM = cv.bitwise_and(inputImg,inputImg, mask= maskImgBinary)
-        error, diffImg = mse(refImgM, inputImgM)
+    while True:
+        inRgb = qRgb.tryGet()  # Non-blocking call, will return a new data that has arrived or None otherwise
+        if inRgb is not None:
+            frame = inRgb.getCvFrame()
+            # 4k / 4
+            frame = cv.pyrDown(frame)
+            frame = cv.pyrDown(frame)
+            # cv.imshow("rgb", frame)
 
-        report.write(str(error))
-        report.write(" Pass") if error < 0.1 else report.write(" Fail")  
-        #for this mask, the set tolerance is 0.1, this seems to work best, but this is experimentally tested (which isn't awesome)
+        if qStill.has():
+            fName = f"{dirName}/{timeStamp()}.jpeg"
+            with open(fName, "wb") as f:
+                f.write(qStill.get().getData())
+                print('Image saved to', fName)
+            time.sleep(3)
+            # compare(fName)
+            os.remove(fName)
+        
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            break
 
-report.close()
+        ctrl = dai.CameraControl()
+        ctrl.setCaptureStill(True)
+        qControl.send(ctrl)
+        print("Sent 'still' event to the camera!")
+        time.sleep(3)
+
+
+# save_path = 'C:/example/'
+
+# name_of_file = raw_input("What is the name of the file: ")
+
+# completeName = os.path.join(save_path, name_of_file+".txt")         
+
+# file1 = open(completeName, "w")
+
+# toFile = raw_input("Write what you want into the field")
+
+# file1.write(toFile)
+
+# file1.close()
+
+
+
+# report.close()
 
