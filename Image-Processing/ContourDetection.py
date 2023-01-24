c67b9192b0189d1fcd176e7eb7357b10756050c7
Eren <aeyilmaz@uwaterloo.ca>
2023-01-24 11:15:26 -0500

    change in edgedetectionfunctiontypes.py file

--- Image-Processing/ContourDetection.py
+++ Image-Processing/ContourDetection.py
@@ -2,15 +2,18 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
 # object = cv2.createBackgroundSubtractorM0G2(history, varThreshold, detectShadows)
-# varThreshold detects squared distance between pixel to decide if pixel is apart of background model. Default value is 16.
+
 
 #Load a video
 cap = cv2.VideoCapture('Photos/IMG_4965.mp4')
 
 # you can optionally work on the live web cam
 
-# create the background object, if shadow is not detected then shown as gray.
+# create the background object, if shadow is not detected then shown as gray. 
+# History controls how many images stored for foreground. (Default value = 500.)
+# varThreshold detects squared distance between pixel to decide if pixel is apart of background model. (Default value = 16)
 backgroundobject = cv2.createBackgroundSubtractorMOG2(varThreshold = 100, history = 10, detectShadows = True)
 
 while(cap.isOpened()):
@@ -19,7 +22,6 @@ while(cap.isOpened()):
         break
     else:
 
-        #VISIUAL PURPOSES:
         # apply the background object on each frame
         fgmask = backgroundobject.apply(frame)
 
@@ -41,3 +43,44 @@ while(cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 #____________________________________________________________________
+'''
+
+#Load video
+video = cv2.VideoCapture('Photos/IMG_4965.mp4')
+#Initialize background object
+#Detect shadows seperates entities from their shadows so objects with overlapping shadows do not count as 1, is not visual.
+BackgroundObject = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
+
+while(video.isOpened()):
+    ret, frame = cap.read()
+    if not ret:
+        break
+    else:
+        #Apply the background object on the frame to get the segmented mask.
+        fgmask = BackgroundObject.apply(frame)
+
+        #Array of numerical co-efficents for pixels in an image. None is default 3x3 size.
+        kernel = None
+
+        #Since shadows are masked as grey and objects as white. We can set threshold to ignore grey.
+        _, fgmask = cv2.threshold(fgmask, 250, 255, cv2.TRESH_BINARY)
+
+        #Dilation increases white area of figures
+        #Erode increases black area of mask
+        fgmask = cv2.erode(fgmask, kernel, iterations = 1)
+        fgmask = cv2.dilate(fgmask, kernel, iterations = 2)
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
