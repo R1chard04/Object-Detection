7ac7411c68aa0a015d960f8a66658acee2a303cc
Henro1708 <henriqueengelke@gmail.com>
2023-01-26 16:36:48 -0500

    Message

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -7,5 +7,5 @@ const element = <h1>Martinrea </h1>;
 
 ReactDOM.render(<Martinrea />, document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<h1>Hello, world!</h1>);
