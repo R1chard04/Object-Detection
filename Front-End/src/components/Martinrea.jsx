7e215b776f466d9718fda4f964c61d12fdd7ea52
Henro1708 <henriqueengelke@gmail.com>
2023-01-26 14:46:57 -0500

    frontened

--- Front-End/src/components/Martinrea.jsx
+++ Front-End/src/components/Martinrea.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import STANDARD from "../STANDARD.jpg";
 
 class Martinrea extends Component {
   state = {
     count: 0,
     imgLogo: "https://camsc.ca/wp-content/uploads/2021/06/Martinrea-logo.png",
-    imgPicture:
-      "C:/Users/henrique.engelke/Desktop/Martinrea/Object-Detection/Image_Processing/photosInput/20230124110825.jpeg", // Put camera pic here
+    imgPicture: "STANDARD", // Put camera pic here
 
     goodOrNot: 1, //This decides if position is good or bad
   };
@@ -15,23 +15,18 @@ class Martinrea extends Component {
   };
   goodOrBad = () => {
     if (this.state.goodOrNot === 1)
-      return <p className=".text-success">GOOD</p>;
-    return <p className=".text-danger">FAILURE</p>;
+      return <h1 className="text-success">GOOD</h1>;
+    return <h1 className="text-danger">FAILURE</h1>;
   };
 
   render() {
     return (
-      <div class="bg-danger position-relative">
+      <div class="bg-primary position-relative">
         <div>
           <img src={this.state.imgLogo} alt="image" height="100" width="100" />
         </div>
         <div>
-          <img
-            src={this.state.imgPicture}
-            alt="image-main"
-            height="576"
-            width="720"
-          />
+          <img src={STANDARD} alt="image-main" height="576" width="720" />
 
           <div className="position-absolute top-50 start-50">
             {this.goodOrBad()}
