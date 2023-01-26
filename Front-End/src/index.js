13b9e5d9db896165c1f3c023c3a425c53bf63b91
Leoy456 <leoyou2004@gmail.com>
2023-01-26 14:40:42 -0500

    garbage

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -6,3 +6,6 @@ import Martinrea from "./components/Martinrea";
 const element = <h1>Martinrea </h1>;
 
 ReactDOM.render(<Martinrea />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
