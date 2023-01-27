8992b621b3de5a8e711d5a4258e937ba9605a323
Leoy456 <leoyou2004@gmail.com>
2023-01-27 11:29:00 -0500

    LOVE YOU KENT

--- Object_Detection/imageCaptureClasses.py
+++ Object_Detection/imageCaptureClasses.py
@@ -59,15 +59,11 @@ class imageCapture:
             imgPath = f"{self.directoryName}/{now}.jpg"
 
             if inRgb is not None:
-                print("here2")
                 img = inRgb.getCvFrame()
                 img = cv.pyrDown(img)
                 img = cv.pyrDown(img) #comment out later
-                done = True
 
             if self.qStill.has():
-                print("here2")
-                
                 with open(imgPath, "wb") as f:
                     f.write(self.qStill.get().getData())
                     print('Image saved to', imgPath)
