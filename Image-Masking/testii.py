733b8a398d90b2aedc6aac26bddc5f0b09284794
YuChiehY <109325354+YuChiehY@users.noreply.github.com>
2023-01-27 11:29:01 -0500

    imageMaskGeneration update

--- Image-Masking/testii.py
+++ Image-Masking/testii.py
@@ -1,1 +1,43 @@
-print ("Press \'s\' to capture a standard photo that has parts on \nPress \'w\' to capture a photo that does not have parts on \nPress \'g\' to generate a mask")
+import time
+import threading
+from functools import wraps
+# # seconds passed since epoch
+# seconds = 1672215379.5045543
+
+# # convert the time in seconds since the epoch to a readable format
+# local_time = time.ctime(seconds)
+
+# print("Local time:", local_time)
+
+# preTime = time.time() +1
+# print(time.time())
+# print(preTime)
+
+while True:
+    # preTime = time.time() +1
+    # if time.time() > preTime:
+    #     print("heyhey")
+    print(time.time()%1//0.2)
+
+# print(0.43243//0.1)
+
+# def delay(delay=0.): 
+#     """ 
+#     Decorator delaying the execution of a function for a while. 
+#     """ 
+#     def wrap(f): 
+#         @wraps(f) 
+#         def delayed(*args, **kwargs): 
+#             timer = threading.Timer(delay, f, args=args, kwargs=kwargs) 
+#             timer.start() 
+#         return delayed 
+#     return wrap 
+
+# from utils import delay 
+ 
+# @delay(3.0) 
+# def my_func(arg1, arg2): 
+#     print arg1, arg2 
+ 
+# if __name__ == '__main__': 
+#     my_func('Hello', 'world') 
