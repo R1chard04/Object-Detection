59f7980fd8f11874e7d986e8b164a65e43fe28ed
YuChiehY <109325354+YuChiehY@users.noreply.github.com>
2023-01-30 08:34:09 -0500

    Jan 27th changes

--- Image-Masking/testii.py
+++ Image-Masking/testii.py
@@ -2,58 +2,11 @@ import time
 
 import threading
 from functools import wraps
-# # seconds passed since epoch
-# seconds = 1672215379.5045543
 
-# # convert the time in seconds since the epoch to a readable format
-# local_time = time.ctime(seconds)
-
-# print("Local time:", local_time)
-
-# preTime = time.time() +1
-# print(time.time())
-# print(preTime)
+# start = time.time()
 
 # while True:
-#     # preTime = time.time() +1
-#     # if time.time() > preTime:
-#     #     print("heyhey")
-#     print(time.time()%1//0.2)
+#     if time.time() - start > 1:
+#         print(time.time())
+#         start = time.time()
 
-# print(0.43243//0.1)
-
-# def delay(delay=0.): 
-#     """ 
-#     Decorator delaying the execution of a function for a while. 
-#     """ 
-#     def wrap(f): 
-#         @wraps(f) 
-#         def delayed(*args, **kwargs): 
-#             timer = threading.Timer(delay, f, args=args, kwargs=kwargs) 
-#             timer.start() 
-#         return delayed 
-#     return wrap 
-
-# from utils import delay 
- 
-# @delay(3.0) 
-# def my_func(arg1, arg2): 
-#     print arg1, arg2 
- 
-# if __name__ == '__main__': 
-#     my_func('Hello', 'world') 
-
-# def takeFrame:
-#     print(time.time())
-
-
-
-# while True:
-#     schedule.run_pending()
-
-start = time.time()
-
-while True:
-    if time.time() - start > 1:
-        print(time.time())
-        start = time.time()
