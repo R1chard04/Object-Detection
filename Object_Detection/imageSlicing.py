4c23a45c4a353c670e662b67c8a18325e6c267d0
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2023-02-07 09:40:05 -0500

    slicing.py

--- Object_Detection/imageSlicing.py
+++ Object_Detection/imageSlicing.py
@@ -21,72 +21,86 @@ while len(input_numbers) < 4:
         print("Invalid input. Enter a number from 1 to 4 or enter 'p' to end inputting numbers.")
 
 # Get image height and width
-img = cv2.imread("./NONE.jpg")
+# img = cv2.imread("./NONE.jpg")
 
-height = img.shape[0]
-width = img.shape[1]
+img = cv2.VideoCapture(0)
 
-#Slice image into 4 quadrants
-width_cutoff = width // 2
-height_cutoff = height // 2
-image_topleft = img[:height_cutoff, :width_cutoff]
-image_topright = img[:height_cutoff, width_cutoff:]
-image_bottomleft = img[height_cutoff:, :width_cutoff]
-image_bottomright = img[height_cutoff:, width_cutoff:]
+def is_frame_running(capture):
+    # Read the next frame from the video capture object
+    ret, frame = capture.read()
 
-# Assign a quadrant to each number 1 to 4
-quadrant_map = {
-    1: image_topleft,
-    2: image_topright,
-    3: image_bottomleft,
-    4: image_bottomright,
-}
-
-# Convert user input to result
-result = []
-non_chosen_images = []
-for i in range(1, 5):
-    if i in input_numbers:
-        result.append(quadrant_map[i])
+    # If the frame was successfully read, return True
+    if ret:
+        return True
+    # If the frame was not successfully read, return False
     else:
-        if i == 1:
-            non_chosen_image = img[:height_cutoff, :width_cutoff]
-        elif i == 2:
-            non_chosen_image = img[:height_cutoff, width_cutoff:]
-        elif i == 3:
-            non_chosen_image = img[height_cutoff:, :width_cutoff]
+        return False
+
+while is_frame_running(img):
+    height = img.shape[0]
+    width = img.shape[1]
+
+    #Slice image into 4 quadrants
+    width_cutoff = width // 2
+    height_cutoff = height // 2
+    image_topleft = img[:height_cutoff, :width_cutoff]
+    image_topright = img[:height_cutoff, width_cutoff:]
+    image_bottomleft = img[height_cutoff:, :width_cutoff]
+    image_bottomright = img[height_cutoff:, width_cutoff:]
+
+    # Assign a quadrant to each number 1 to 4
+    quadrant_map = {
+        1: image_topleft,
+        2: image_topright,
+        3: image_bottomleft,
+        4: image_bottomright,
+    }
+
+    # Convert user input to result
+    result = []
+    non_chosen_images = []
+    for i in range(1, 5):
+        if i in input_numbers:
+            result.append(quadrant_map[i])
         else:
-            non_chosen_image = img[height_cutoff:, width_cutoff:]
-        non_chosen_images.append(non_chosen_image)
+            if i == 1:
+                non_chosen_image = img[:height_cutoff, :width_cutoff]
+            elif i == 2:
+                non_chosen_image = img[:height_cutoff, width_cutoff:]
+            elif i == 3:
+                non_chosen_image = img[height_cutoff:, :width_cutoff]
+            else:
+                non_chosen_image = img[height_cutoff:, width_cutoff:]
+            non_chosen_images.append(non_chosen_image)
 
-# Combine non-chosen images
-if len(non_chosen_images) == 2:
-    if 1 in input_numbers and 2 in input_numbers:
-        result.append(cv2.hconcat(non_chosen_images))
-    elif 3 in input_numbers and 4 in input_numbers:
-        result.append(cv2.hconcat(non_chosen_images))
-    elif 1 in input_numbers and 3 in input_numbers:
-        result.append(cv2.vconcat(non_chosen_images))
-    elif 2 in input_numbers and 4 in input_numbers:
-        result.append(cv2.vconcat(non_chosen_images))
-        
-    # Get aspect ratio of the quadrant
-    aspect_ratio = height / width
+    # Combine non-chosen images
+    if len(non_chosen_images) == 2:
+        if 1 in input_numbers and 2 in input_numbers:
+            result.append(cv2.hconcat(non_chosen_images))
+        elif 3 in input_numbers and 4 in input_numbers:
+            result.append(cv2.hconcat(non_chosen_images))
+        elif 1 in input_numbers and 3 in input_numbers:
+            result.append(cv2.vconcat(non_chosen_images))
+        elif 2 in input_numbers and 4 in input_numbers:
+            result.append(cv2.vconcat(non_chosen_images))
+            
+        # Get aspect ratio of the quadrant
+        aspect_ratio = height / width
 
-    # Get height of combined image
-    combined_height = int(result[0].shape[1] * aspect_ratio)
+        # Get height of combined image
+        combined_height = int(result[0].shape[1] * aspect_ratio)
 
-    # Resize the combined image to have the same aspect ratio as the quadrant but different width to fit everything
-    result[0] = cv2.resize(result[0], (result[0].shape[1], combined_height),
-                       interpolation=cv2.INTER_CUBIC)
+        # Resize the combined image to have the same aspect ratio as the quadrant but different width to fit everything
+        result[0] = cv2.resize(result[0], (result[0].shape[1], combined_height),
+                        interpolation=cv2.INTER_CUBIC)
 
-# Use cv.imshow to output the result
-for i, res in enumerate(result):
-    window_height = int(res.shape[0] * 0.7)
-    window_width = int(res.shape[1] * 0.7)
-    cv2.namedWindow(f"Quadrant {i+1}", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(f"Quadrant {i+1}", window_width, window_height)
-    cv2.imwrite(f"./Quadrant {i+1}", res)
+    # Use cv.imshow to output the result
+    for i, res in enumerate(result):
+        window_height = int(res.shape[0] * 0.7)
+        window_width = int(res.shape[1] * 0.7)
+        cv2.namedWindow(f"Quadrant {i+1}", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(f"Quadrant {i+1}", window_width, window_height)
+        cv2.imshow(f"./Quadrant {i+1}", res)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        cv2.waitKey(10)
+        cv2.destroyAllWindows()
