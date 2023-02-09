c7ae85b91b75f335783ef39e59dcea84f457f16b
Eren <aeyilmaz@uwaterloo.ca>
2023-02-09 09:06:57 -0500

    changes

--- Object_Detection/sliceimages.py
+++ Object_Detection/sliceimages.py
@@ -1,10 +1,10 @@
 from imageSlicing import imageSlicing
 import cv2 as cv
 
-img = cv.imread("")
+img = cv.imread("./mask.jpg")
 
 img_slicer = imageSlicing(img)
 result = img_slicer.imageSlicing()
 
 for i, res in enumerate(result):
-    cv.imwrite(f"./Quadrant {i+1}", res)
+    cv.imwrite(f"./Quadrant {i+1}.jpg", res)
