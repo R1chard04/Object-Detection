dcfaebd6303fe77608876495cb076b66fe7b3846
Henrique Engelke Rodrigues <henrique.engelke@BRAMPTON.MARTINREA.COM>
2023-02-09 10:28:41 -0500

    Masks

--- Object_Detection/sliceimages.py
+++ Object_Detection/sliceimages.py
@@ -1,14 +1,10 @@
 from imageSlicing import imageSlicing
 import cv2 as cv
 
-<<<<<<< HEAD
-img = cv.imread("")
-=======
-img = cv.imread("./mask.jpg")
->>>>>>> c7ae85b91b75f335783ef39e59dcea84f457f16b
+img = cv.imread("Object_Detection/Photos/MASKS/1675868011079.jpg")
 
 img_slicer = imageSlicing(img)
 result = img_slicer.imageSlicing()
 
 for i, res in enumerate(result):
-    cv.imwrite(f"./Quadrant {i+1}.jpg", res)
+    cv.imwrite(f"./Quadrant {i+3}.jpg", res)
