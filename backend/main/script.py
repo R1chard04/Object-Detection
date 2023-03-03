48adf51b3bd0898bf08ce789b832c86b46ba9e7e
Leo <108024684+Leoy456@users.noreply.github.com>
2023-03-03 13:48:36 -0500

    Multiprocessing

--- backend/main/script.py
+++ backend/main/script.py
@@ -1,10 +1,24 @@
 import concurrent.futures
 import time
 from main import mainloop
-
+import multiprocessing
 # selected, recalibrate  = fromplc()
 selected = [0,1]
 
 if __name__ == '__main__':    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(mainloop, selected)
+
+    process1 = multiprocessing.Process(target=mainloop, args = 0)
+    process2 = multiprocessing.Process(target=mainloop, args = 1)
+    process3 = multiprocessing.Process(target=mainloop, args = 2)
+
+    # Start the processes
+    process1.start()
+    process2.start()
+    process3.start()
+    
+    # Wait for the processes to finish
+    process1.join()
+    process2.join()
+    process3.join()
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         executor.map(mainloop, selected)
