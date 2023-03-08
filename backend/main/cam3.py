67b92ba9c5719372ebf79490be983592169ec080
YuChiehY <jamiejamie920319@gmail.com>
2023-03-08 14:32:22 -0500

    multi camera done

--- backend/main/cam3.py
+++ backend/main/cam3.py
@@ -6,112 +6,35 @@ from imageProcessingClasses import imageProcessing
 from imageCaptureClasses import imageCapture
 from imageMaskGeneration import createMask
 from imagePredictionClass import MSEStabilization, getPassRef
+from calibrations import Recalibration, createPipeline
 import time
 import os
 import json
-import pdb
+import cv2 as cv
+import depthai as dai
 
-# from pylogix import PLC
-# from PLCUpdate import transferToPLC
+camera = Recalibration("station10")
+processingObject = imageProcessing("station10")
 
-#-----------------------------------------Directories-----------------------------------------#
-#Photos Path
-photosPath = "Photos\Init" #This is where all the images are being saved
+device_info = dai.DeviceInfo(camera.IP)
 
-#MSE Initialisation Photos Path
-errDir = "Photos\Err" #This is whered the difference images can be saved; they are not currently
-
-#Mask Paths
-refDir = "Photos\Refs" #This is where the reference photos for each part at one station are being saved
-colDir = "Photos\Col" #This is where the alternate colour reference photos for each part at one station are being saved
-maskDir = "Photos\Masks" #This is where the generated masks are being saved
-
-#Part List
-with open(r'params.json') as f:
-  partList = json.load(f)
-
-# select which station to use here
-params = partList["station10"]
-
-brightness = params["brightness"]
-lensPos = params["lensPos"]
-parts = params["parts"]
-passref = params["passref"]
-IP = params["IP"]
-maskPaths = params["masks"]
-refPaths = params["refs"]
-colPaths = params ["cols"]
-standardPath = params["standard"]
-testPath = params["test"]
-
-# for n in maskPaths:
-#     print(n)
-# print(refPaths[0])
-# img = cv.imread(maskPaths[0])
-# cv.imshow('hi',img)
-# cv.waitKey(0)
-
-# Create pipeline
-pipeline = dai.Pipeline()
-# This might improve reducing the latency on some systems
-pipeline.setXLinkChunkSize(0)
-
-# Define source and output
-camRgb = pipeline.create(dai.node.ColorCamera)
-camRgb.setFps(3)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-camRgb.setPreviewSize(1920, 1080)
-
-xout = pipeline.create(dai.node.XLinkOut)
-xout.setStreamName("out")
-camRgb.isp.link(xout.input)
-
-device_info = dai.DeviceInfo(IP)
-
-with dai.Device(pipeline, device_info) as device:
-    q = device.getOutputQueue(name="out")
-
-    print("here4")
+with dai.Device(createPipeline(), device_info) as device:
+    camera.adjustCamera(device)
     
-    print("done params")
-
-    # masks = maskSetup(selected, captureObject, recalibrate, brightness, lensPos, IP)
-    masks = []
-    for path in maskPaths:
-        masks.append(cv.imread(path, 0))
-    print("done masks")
-    # tempRef = controlSetup(selected, captureObject, recalibrate, brightness, lensPos)
-    tempRef = cv.imread(standardPath)
-    # pdb.set_trace()
-    processingObject = imageProcessing(masks, tempRef, tempRef, parts) #Initialisation of the processing object
-    print("processing object initialized")
-
-    # ref, passref = errorSetup(selected, captureObject, processingObject, recalibrate, brightness, lensPos)
-    # print("errorsetup")
-    # processingObject.setRefImg(tempRef)
-    # print("refsetup")
-
-#-------------------------------------------------------------------------------------------#   
-    while True:
+    print("start")
+    while True:     
         
-        imgFrame = q.get()
-        print(IP)
-        # capture a test image
-        # img = captureObject.captureOne(testPath, brightness, lensPos)
-        img = imgFrame.getCvFrame()
-        processingObject.setTestImg(img)  
-        # display the result on the frame
+        img = camera.capture(device)
+        processingObject.setTestImg(img)
         frame = processingObject.displayResultPosition()
-        # get the mse error
-        error = processingObject.compareImage() 
-        #Generates PASS/FAIL array
-        prediction = MSEStabilization(error, passref, len(parts)) 
+        error = processingObject.compareImage()
+        prediction = MSEStabilization(error, camera.passref, len(camera.parts)) 
 
         result = prediction.result()
-        # print(result)
+        print(result)
 
         # transferToPLC("OP100", result)
         cv.waitKey(1)
         frame = cv.pyrDown(frame)
         frame = cv.pyrDown(frame)
-        cv.imshow(IP, frame)
+        cv.imshow(camera.IP, frame)
--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from main.calibrations import Recalibration, createPipeline
-import cv2 as cv
-import depthai as dai
-camera = Recalibration("station10")
-
-device_info = dai.DeviceInfo(camera.IP)
-with dai.Device(createPipeline(), device_info) as device:
-    camera.adjustCamera(device)
