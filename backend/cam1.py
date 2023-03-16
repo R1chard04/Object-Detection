078565779f8a7c6376651891ad78e1563478514a
Eren Yilmaz <eren.yilmaz@BRAMPTON.MARTINREA.COM>
2023-03-16 10:54:16 -0400

    changes

--- backend/cam1.py
+++ backend/cam1.py
@@ -4,33 +4,30 @@ import depthai as dai
 from imageProcessingClasses import imageProcessing
 from imagePredictionClass import MSEStabilization
 from imageCalibrationClass import Recalibration, createPipeline
-# from imageTimingClasses import imageTiming
-# from imageAverageClasses import imageAverage
+from imageTimingClasses import imageTiming
+from imageAverageClasses import imageAverage
 import time
 import cv2 as cv
 import depthai as dai
 import pdb
 import pylogix 
 from pylogix import PLC
-from PLCUpdate import writePLC
-from time import timeLog
-    
 from PLCUpdate import writePLC, readPLC
 
-# db_config = {
-#      "hostname": "localhost",
-#      "database": "imageTiming",
-#      "username": "postgres",
-#      "pwd": "W1nter@2023Hydro",
-#      "port_id": 5432
-#  }
+db_config = {
+     "hostname": "localhost",
+     "database": "imageTiming",
+     "username": "postgres",
+     "pwd": "W1nter@2023Hydro",
+     "port_id": 5432
+ }
 
 camera = Recalibration("station120")
 device_info = dai.DeviceInfo(camera.IP)
 
 with dai.Device(createPipeline(), device_info) as device:
     camera.adjustCamera(device)
-    # camera.pressKeyCapture(device, camera.standardPath)
+    camera.pressKeyCapture(device, camera.standardPath)
     camera.errorSetup(device)
     print("final error:")
     print(camera.passref)
@@ -40,15 +37,16 @@ with dai.Device(createPipeline(), device_info) as device:
     # print("start")
     
     # ask user to name each occurance in array
-    # assigned_names = [1, 2, 3, 4]
-    # print(assigned_names)
+    new_partLists120 = ['TopRightPart', 'TopLeftPart', 'LeftPart', 'BottomLeftPart', 'BottomRightPart', 'RightPart']
+    print(new_partLists120)
 
-    # timing = imageTiming(assigned_names, db_config)
+    timing = imageTiming(new_partLists120, db_config)
 
-    arr = [0, 0, 0, 0]
-    timeObject = timeLog(camera.IP, camera['parts'])
-
-    while True:         
+    arr = [0, 0, 0, 0, 0, 0]
+    
+    print("going into loop")
+    
+    while True:     
         
         img = camera.capture(device)
         processingObject.setTestImg(img)
@@ -56,27 +54,19 @@ with dai.Device(createPipeline(), device_info) as device:
         frame = processingObject.displayResultPosition()     
         prediction = MSEStabilization(error, camera.passref, len(camera.parts)) 
         # pdb.set_trace()
+        print(error)
         result = prediction.result()
-
-        clampClosed = readPLC("Program:Sta120.Station.Cycle.Step.Bit[10]")
-
-        recorded = timeObject.record(result, clampClosed)
-
-        # response = timing.record(result)
-        # calculation = imageAverage(db_config)
-        # final = calculation.average()
+        
+        response = timing.record(result)
+        calculation = imageAverage(db_config)
+        final = calculation.average()
         #  # write PLC value to the HMI
-        writePLC("Camera_Output.5", result)
-
+        # writePLC("Camera_Output.1", result)
         print(result)
+        print(final)
 
         # transferToPLC("OP100", result)
         cv.waitKey(1)
         frame = cv.pyrDown(frame)
+        frame = cv.pyrDown(frame)
         cv.imshow(camera.IP, frame)
-
-        if recorded is True:
-            while readPLC("Sta120_OK_To_Enter") is False:
-                pass
-            timeObject = timeLog(camera.IP, camera['parts'])
-                
--- backend/time.py
+++ backend/time.py
@@ -50,5 +50,4 @@ class timeLog:
 
             return False
 
-
         
