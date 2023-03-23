453cd4507db756837b0af5dedd00185af13f9c5e
Kent Tran <kent.tran@BRAMPTON.MARTINREA.COM>
2023-03-23 17:30:13 -0400

    working on css

--- backend/static-js/stations_programs_start.js
+++ backend/static-js/stations_programs_start.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+ const toggleBtn = document.querySelector('.toggle_btn');
+ const toggleBtnIcon = document.querySelector('.toggle_btn i');
+ const dropDownMenu = document.querySelector('.dropdown_menu')
+
+ toggleBtn.addEventListener('click', function() {
+  dropDownMenu.classList.toggle('open')
+ })
+
+ // button run all the cameras
  let btnClick = document.getElementById('start-button');
  function runPrograms() {
   window.location.href = "http://127.0.0.1:5000/bt1xx/startallprograms/";
