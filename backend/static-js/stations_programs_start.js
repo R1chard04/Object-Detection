9893eab7772613dc23ebee93bdb95a043602b173
Kent Tran <kent.tran@BRAMPTON.MARTINREA.COM>
2023-03-24 14:05:59 -0400

    css for homepage

--- backend/static-js/stations_programs_start.js
+++ backend/static-js/stations_programs_start.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
- const toggleBtn = document.querySelector('.toggle_btn');
- const toggleBtnIcon = document.querySelector('.toggle_btn i');
- const dropDownMenu = document.querySelector('.dropdown_menu')
+  const toggleBtn = document.querySelector('.toggle_btn');
+  const toggleBtnIcon = document.querySelector('.toggle_btn i');
+  const dropDownMenu = document.querySelector('.dropdown_menu');
+  
+  toggleBtn.onclick = function () {
+   dropDownMenu.classList.toggle('open');
+   const isOpen = dropDownMenu.classList.contains('open');
 
- toggleBtn.addEventListener('click', function() {
-  dropDownMenu.classList.toggle('open')
- })
-
+   toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+  }
  // button run all the cameras
  let btnClick = document.getElementById('start-button');
  function runPrograms() {
