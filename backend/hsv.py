3fa66bc62bdaec2f9ff2aabb8e1877ea3b713e48
Leo <108024684+Leoy456@users.noreply.github.com>
2023-03-30 10:40:39 -0400

    HSV implemented

--- backend/hsv.py
+++ backend/hsv.py
@@ -12,15 +12,23 @@ def mse(img1, img2, pixels):
 
 img1 = cv.imread("backend\Photos\Col\station100\left.jpg")
 img2 = cv.imread("backend\Photos\Refs\station100\left.jpg")
+mask = cv.imread("backend\Photos\Masks\station100\left.jpg",0)
 
-img1 = cv.cvtColor(img1, cv.COLOR_BGR2HSV)
-img2 = cv.cvtColor(img2, cv.COLOR_BGR2HSV)
+img1 = cv.bitwise_and(img1, img1, mask = mask)
+img2 = cv.bitwise_and(img2, img2, mask = mask)
 
-h1, s1, v1 = cv.split(img1)
-h2, s2, v2 = cv.split(img2)
+# img1 = cv.cvtColor(img1, cv.COLOR_BGR2HSV)
+# img2 = cv.cvtColor(img2, cv.COLOR_BGR2HSV)
 
+# h1, s1, v1 = cv.split(img1)
+# h2, s2, v2 = cv.split(img2)
 
+pixels = img1.size
+print(pixels)
 
-cv.imshow("h1", cv.pyrDown(v1))
-cv.imshow("h2", cv.pyrDown(v2))
+ans = mse(img1,img2,pixels)
+print(ans)
+
+cv.imshow("h1", cv.pyrDown(img1))
+cv.imshow("h2", cv.pyrDown(img2))
 cv.waitKey(0)
