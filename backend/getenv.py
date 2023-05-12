7e6b58fac6dba9cc0a38c94dcd83dbd85c2bc5a1
Richard Miao <richard.miao@BRAMPTON.MARTINREA.COM>
2023-05-12 09:37:07 -0400

    changed getenv.py

--- backend/getenv.py
+++ backend/getenv.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # getting mongodb connection credentials
-mongodb_username = os.getenv('MONGO_DB_USERNAME')
-mongodb_password = os.getenv('MONGO_DB_PASSWORD')
+mongodb_username = os.getenv('MONGO_DB_USERNAME', "mongodb username does not exist")
+mongodb_password = os.getenv('MONGO_DB_PASSWORD', "mongodb password does not exist")
 
 # getting mysql connection credentials
-mysql_username = os.getenv('MYSQL_DB_USERNAME')
-mysql_password = os.getenv('MYSQL_DB_PASSWORD')
-mysql_host = os.getenv('MYSQL_DB_HOST')
-mysql_port = os.getenv('MYSQL_DB_PORT')
-mysql_db_name = os.getenv('MYSQL_DB_NAME')
+mysql_username = os.getenv('MYSQL_DB_USERNAME', "mysql username does not exist")
+mysql_password = os.getenv('MYSQL_DB_PASSWORD', "mysql password does not exist")
+mysql_host = os.getenv('MYSQL_DB_HOST', "mysql host does not exist")
+mysql_port = os.getenv('MYSQL_DB_PORT', "mysql port does not exist")
+mysql_db_name = os.getenv('MYSQL_DB_NAME', "mysql database name does not exist")
